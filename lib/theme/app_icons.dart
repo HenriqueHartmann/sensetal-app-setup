bf99595d36e14965bfa1530cc81677b20ab663a3
HenriqueHartmann <alemaohartmann@gmail.com>
2025-04-24 00:55:04 -0300

    main - add sensetal icon 2

--- lib/theme/app_icons.dart
+++ lib/theme/app_icons.dart
@@ -1,4 +1,5 @@
 class AppIcons {
   static const String sensetalIcon = "assets/icons/sensetal-icon.svg";
+  static const String sensetalIcon2 = "assets/icons/sensetal-icon-2.svg";
   static const String sensetalLogo = "assets/icons/sensetal-logo.svg";
 }
