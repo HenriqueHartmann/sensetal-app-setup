d4bf8ce0b5dc797986d65f13b60bef9c693ad903
HenriqueHartmann <alemaohartmann@gmail.com>
2025-04-26 21:00:05 -0300

    main - add icon of eyes open and closed

--- lib/theme/app_icons.dart
+++ lib/theme/app_icons.dart
@@ -2,4 +2,7 @@ class AppIcons {
   static const String sensetalIcon = "assets/icons/sensetal-icon.svg";
   static const String sensetalIcon2 = "assets/icons/sensetal-icon-2.svg";
   static const String sensetalLogo = "assets/icons/sensetal-logo.svg";
+
+  static const String sensetalIconEyeOpen = "assets/icons/sensetal-icon-eye-open.svg";
+  static const String sensetalIconEyeClosed = "assets/icons/sensetal-icon-eye-closed.svg";
 }
