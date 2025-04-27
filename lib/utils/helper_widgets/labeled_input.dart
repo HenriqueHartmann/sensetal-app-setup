68c803e752d1d17559867e56ba7578e2d4b90fcd
HenriqueHartmann <alemaohartmann@gmail.com>
2025-04-26 21:03:15 -0300

    main - update LabeledInput widget to Statefull and add obscure option

--- lib/utils/helper_widgets/labeled_input.dart
+++ lib/utils/helper_widgets/labeled_input.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sensetal_presentation_design_app/theme/app_colors.dart';
+import 'package:sensetal_presentation_design_app/theme/app_icons.dart';
 import 'package:sensetal_presentation_design_app/theme/app_space_size.dart';
 import 'package:sensetal_presentation_design_app/utils/helper_widgets/space_widgets.dart';
 
-class LabeledInput extends StatelessWidget {
+class LabeledInput extends StatefulWidget {
   final String label;
   final String placeholder;
   final TextEditingController? controller;
+  final bool showSuffixIcon;
 
   const LabeledInput({
     super.key,
     required this.label,
     required this.placeholder,
     this.controller,
+    this.showSuffixIcon = false,
   });
+
+  @override
+  State<LabeledInput> createState() => _LabeledInputState();
+}
+
+class _LabeledInputState extends State<LabeledInput> {
+  late bool isObscure;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isObscure = widget.showSuffixIcon;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    if (widget.controller != null) {
+      widget.controller!.dispose();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +48,7 @@ class LabeledInput extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          label,
+          widget.label,
           style: Theme.of(context).textTheme.bodySmall?.copyWith(
                 color: AppColors.neutral03,
               ),
@@ -39,13 +66,43 @@ class LabeledInput extends StatelessWidget {
             borderRadius: BorderRadius.circular(8), // radius
           ),
           child: TextField(
-            controller: controller,
+            controller: widget.controller,
+            obscureText: isObscure,
+            obscuringCharacter: '*',
             decoration: InputDecoration(
-              hintText: placeholder,
+              hintText: widget.placeholder,
               hintStyle: const TextStyle(color: AppColors.neutral04),
               border: InputBorder.none,
               isDense: true,
               contentPadding: EdgeInsets.zero,
+              suffixIconConstraints:
+                  const BoxConstraints(minHeight: 24, minWidth: 24),
+              suffixIcon: widget.showSuffixIcon
+                  ? SizedBox(
+                      height: 24,
+                      width: 24 + getSizeFromEnum(AppSpaceSize.sm),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: getSizeFromEnum(AppSpaceSize.sm),
+                        ),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isObscure = !isObscure;
+                            });
+                          },
+                          child: SvgPicture.asset(
+                            isObscure
+                                ? AppIcons.sensetalIconEyeClosed
+                                : AppIcons.sensetalIconEyeOpen,
+                            semanticsLabel: 'Sensetal icon for action',
+                            // ignore: deprecated_member_use
+                            color: AppColors.neutral03,
+                          ),
+                        ),
+                      ),
+                    )
+                  : null,
             ),
             style: Theme.of(context).textTheme.bodyMedium,
           ),
