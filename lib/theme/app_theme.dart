baa5daebe043cc0305a55f84dfb92e761d12aec1
HenriqueHartmann <alemaohartmann@gmail.com>
2025-05-02 22:16:12 -0300

    main - remove colors from theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,18 +5,5 @@ import 'package:sensetal_presentation_design_app/theme/app_text_theme.dart';
 final ThemeData appTheme = ThemeData(
   useMaterial3: true,
   scaffoldBackgroundColor: AppColors.neutralWhite,
-  colorScheme: const ColorScheme(
-    brightness: Brightness.light,
-    primary: AppColors.primary02,
-    onPrimary: AppColors.neutralWhite,
-    secondary: AppColors.secondary02,
-    onSecondary: AppColors.neutral01,
-    background: AppColors.neutral05,
-    onBackground: AppColors.neutral01,
-    surface: AppColors.neutralWhite,
-    onSurface: AppColors.neutral01,
-    error: AppColors.error02,
-    onError: AppColors.neutralWhite,
-  ),
   textTheme: appTextTheme,
 );
