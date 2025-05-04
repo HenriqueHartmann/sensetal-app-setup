da5b70a3af4e354f31a7e7180dd42fec748635a0
HenriqueHartmann <alemaohartmann@gmail.com>
2025-05-03 21:34:49 -0300

    main - adjust brandSensetalIconVariant path in AppIcons

--- lib/theme/app_icons.dart
+++ lib/theme/app_icons.dart
@@ -1,6 +1,6 @@
 class AppIcons {
   static const String brandSensetalIcon = "assets/icons/brand/sensetal-icon.svg";
-  static const String brandSensetalIconVariant = "assets/icons/brand/sensetal-icon-2.svg";
+  static const String brandSensetalIconVariant = "assets/icons/brand/sensetal-icon-variant.svg";
   static const String brandSensetalLogo = "assets/icons/brand/sensetal-logo.svg";
 
   static const String systemSensetalIconEyeOpen = "assets/icons/system/sensetal-icon-eye-open.svg";
