48c45c7edcfd69708f1ac8512a0604dc7ba8c0fe
HenriqueHartmann <alemaohartmann@gmail.com>
2025-05-04 00:28:25 -0300

    main - update SensetalScaffold Widget with renamed props and new ones

--- lib/components/sensetal_scaffold.dart
+++ lib/components/sensetal_scaffold.dart
@@ -3,20 +3,21 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sensetal_presentation_design_app/theme/app_icons.dart';
 import 'package:sensetal_presentation_design_app/theme/app_space_size.dart';
 import 'package:sensetal_presentation_design_app/utils/helper_widgets/blurred_background.dart';
-import 'package:sensetal_presentation_design_app/utils/helper_widgets/space_widgets.dart';
 
 class SensetalScaffold extends StatelessWidget {
   final Widget child;
-  final bool showLogo;
-  final bool showSensetalIcon;
-  final double? customVerticalGap;
+  final bool showSensetalLogoInTop;
+  final bool sensetalLogoIsMini;
+  final bool showSensetalIconInBackground;
+  final double? customVerticalPadding;
 
   const SensetalScaffold({
     super.key,
     required this.child,
-    this.showLogo = true,
-    this.showSensetalIcon = true,
-    this.customVerticalGap,
+    this.showSensetalLogoInTop = true,
+    this.sensetalLogoIsMini = true,
+    this.showSensetalIconInBackground = true,
+    this.customVerticalPadding,
   });
 
   @override
@@ -24,46 +25,37 @@ class SensetalScaffold extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: BlurredBackground(
-          showSensetalIcon: showSensetalIcon,
+          showSensetalIconInBackground: showSensetalIconInBackground,
           child: LayoutBuilder(
             builder: (context, constraints) {
               double screenWidth = constraints.maxWidth;
-              double screenHeight = constraints.maxHeight;
 
-              double logoWidth = screenWidth * 0.43;
-              double verticalGap = customVerticalGap ??
-                  (screenHeight > 800
-                      ? 130.0
-                      : screenHeight > 600
-                          ? screenHeight * 0.17
-                          : screenHeight * 0.12);
+              double logoWidth =
+                  sensetalLogoIsMini ? screenWidth * 0.27 : screenWidth * 0.43;
 
-              double horizontalPadding = screenWidth < 400
+              double horizontalPadding = screenWidth < 360
                   ? getSizeFromEnum(AppSpaceSize.md)
-                  : screenWidth < 800
-                      ? getSizeFromEnum(AppSpaceSize.xl)
-                      : getSizeFromEnum(AppSpaceSize.custom, 64.0);
+                  : screenWidth < 600
+                      ? getSizeFromEnum(AppSpaceSize.lg)
+                      : getSizeFromEnum(AppSpaceSize.xxl);
+              double verticalPadding = customVerticalPadding != null
+                  ? customVerticalPadding!
+                  : horizontalPadding;
 
               return SingleChildScrollView(
                 child: Padding(
-                  padding: EdgeInsets.only(
-                    top: getSizeFromEnum(AppSpaceSize.custom, 64.0),
-                    bottom: getSizeFromEnum(AppSpaceSize.xl),
-                    left: horizontalPadding,
-                    right: horizontalPadding,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: horizontalPadding,
+                    vertical: verticalPadding,
                   ),
                   child: Column(
                     children: [
-                      if (showLogo)
+                      if (showSensetalLogoInTop)
                         SvgPicture.asset(
                           AppIcons.brandSensetalLogo,
                           width: logoWidth,
                           semanticsLabel: 'Sensetal logo',
                         ),
-                      VerticalSpace(
-                        size: AppSpaceSize.custom,
-                        custom: verticalGap,
-                      ),
                       child,
                     ],
                   ),
