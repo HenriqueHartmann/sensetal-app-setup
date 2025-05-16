eeb0124a06a1454022a38f471c5bfda3575bf91d
Alex Cyrillo <alex.csborges@hotmail.com>
2025-05-16 19:50:58 -0300

    incluido novos icones

--- lib/theme/app_icons.dart
+++ lib/theme/app_icons.dart
@@ -1,10 +1,19 @@
 class AppIcons {
-  static const String brandSensetalIcon = "assets/icons/brand/sensetal-icon.svg";
-  static const String brandSensetalIconVariant = "assets/icons/brand/sensetal-icon-variant.svg";
-  static const String brandSensetalLogo = "assets/icons/brand/sensetal-logo.svg";
+  static const String brandSensetalIcon =
+      "assets/icons/brand/sensetal-icon.svg";
+  static const String brandSensetalIconVariant =
+      "assets/icons/brand/sensetal-icon-variant.svg";
+  static const String brandSensetalLogo =
+      "assets/icons/brand/sensetal-logo.svg";
 
-  static const String systemSensetalIconEyeOpen = "assets/icons/system/sensetal-icon-eye-open.svg";
-  static const String systemSensetalIconEyeClosed = "assets/icons/system/sensetal-icon-eye-closed.svg";
-  static const String systemSensetalIconCheckMark = "assets/icons/system/sensetal-icon-check-circle.svg";
-  static const String systemSensetalIconXCircle = "assets/icons/system/sensetal-icon-x-circle.svg";
+  static const String systemSensetalIconEyeOpen =
+      "assets/icons/system/sensetal-icon-eye-open.svg";
+  static const String systemSensetalIconEyeClosed =
+      "assets/icons/system/sensetal-icon-eye-closed.svg";
+  static const String systemSensetalIconCheckMark =
+      "assets/icons/system/sensetal-icon-check-circle.svg";
+  static const String systemSensetalIconXCircle =
+      "assets/icons/system/sensetal-icon-x-circle.svg";
+  static const String systemSensetalIconClockwiseArrows =
+      "assets/icons/system/sensetal-icon-clockwise-arrows.svg";
 }
